657e627de56ddab63bbad7e3e54513b273634cb4
Bernard Gingold <beniekg@gmail.com>
2025-12-03 14:58:11 +0100

    Added: LUT arrays for loop indices.

---
+++ src/Math/GMS_loop_indices_LUT.h
@@ -0,0 +1,53 @@
+
+
+
+
+#ifndef __GMS_LOOP_INDICES_LUT_H__
+#define __GMS_LOOP_INDICES_LUT_H__ 031220250814 
+
+
+namespace gms 
+{
+
+namespace math 
+{
+
+/*
+     Loop indices LUT ranging from 0,...2256
+*/
+extern const float LUT_loop_indices_2257_align16[2257];
+
+extern const float LUT_loop_indices_2257_align32[2257];
+
+extern const float LUT_loop_indices_2257_align64[2257];
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /*__GMS_LOOP_INDICES_LUT_H__*/
