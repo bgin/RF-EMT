1d5a6933305d85397124c304bf7b62fd2e163284
Bernard Gingold <beniekg@gmail.com>
2026-07-27 09:25:33 +0200

    Fixed a few errors.

--- src/Math/GMS_cephes_sin_cos.h
+++ src/Math/GMS_cephes_sin_cos.h
@@ -6,23 +6,22 @@ Copyright 1984, 1987, 1989 by Stephen L. Moshier
 Direct inquiries to 30 Frost Street, Cambridge, MA 02140
 */
 
-/*MIT License
-!Copyright (c) 2020 Bernard Gingold
-!Permission is hereby granted, free of charge, to any person obtaining a copy
-!of this software and associated documentation files (the "Software"), to deal
-!in the Software without restriction, including without limitation the rights
-!to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-!copies of the Software, and to permit persons to whom the Software is
-!furnished to do so, subject to the following conditions:
-!The above copyright notice and this permission notice shall be included in all
-!copies or substantial portions of the Software.
-!THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-!IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-!FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-!AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-!LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-!OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-!SOFTWARE.
+/*
+ * Copyright (C) Bernard Gingold, 2020-2026 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
 #ifndef __GMS_CEPHES_SIN_COS_H__
@@ -30,6 +29,7 @@ Direct inquiries to 30 Frost Street, Cambridge, MA 02140
 
 
 #include <limits>
+#include <cstdint>
 #include "GMS_config.h"
 
 
@@ -120,6 +120,7 @@ else
 	y -= 0.5 * z;
 	y += 1.0;
 	}
+
 if(sign < 0)
 	y = -y;
 return( y );
@@ -343,6 +344,108 @@ if( e < 1 )
 return(u.y);
 }
 
+__ATTR_ALWAYS_INLINE__
+static inline
+float ceph_floorf( float x ) 
+{
+
+/* Bit clearing masks: */
+
+static unsigned short bmask[] = 
+{
+       0xffff,
+       0xfffe,
+       0xfffc,
+       0xfff8,
+       0xfff0,
+       0xffe0,
+       0xffc0,
+       0xff80,
+       0xff00,
+       0xfe00,
+       0xfc00,
+       0xf800,
+       0xf000,
+       0xe000,
+       0xc000,
+       0x8000,
+       0x0000,
+    };
+constexpr std::int32_t NBITS = 24;
+unsigned short *p;
+union
+  {
+    float y;
+    unsigned short i[2];
+  } u;
+int e;
+u.y = x;
+/* find the exponent (power of 2) */
+p = &u.i[1];
+e = (( *p >> 7) & 0xff) - 0x7f;
+p -= 1;
+if( e < 0 ){
+    if( u.y < 0 )
+	 return( -1.0 );
+    else
+	 return( 0.0 );
+}
+e = (NBITS -1) - e;
+/* clean out 16 bits at a time */
+while( e >= 16 ) {
+	*p++ = 0;
+	 e -= 16;
+}
+
+/* clear the remaining bits */
+if( e > 0 )
+	*p &= bmask[e];
+
+if( (x < 0) && (u.y != x) )
+	u.y -= 1.0;
+
+   return(u.y);
+}
+
+__ATTR_ALWAYS_INLINE__
+static inline
+float ceph_expf(const float xx) {
+float x, z;
+int n;
+x = xx;
+if( x > 88.72283905206835f) {
+    return(3.4028234663852885981170418348451692544e38f);
+}
+if( x < -103.278929903431851103f) {
+    return(0.0);
+}
+
+/* Express e**x = e**g 2**n
+ *   = e**g e**( n loge(2) )
+ *   = e**( g + n loge(2) )
+ */
+z = ceph_floorf(1.44269504088896341f * x + 0.5 ); /* floor() truncates toward -infinity. */
+x -= z * 0.693359375f;
+x -= z * -2.12194440e-4f;
+n = z;
+
+z = x * x;
+/* Theoretical peak relative error in [-0.5, +0.5] is 4.2e-9. */
+z =
+((((( 1.9875691500E-4f  * x
+   + 1.3981999507E-3f) * x
+   + 8.3334519073E-3f) * x
+   + 4.1665795894E-2f) * x
+   + 1.6666665459E-1f) * x
+   + 5.0000001201E-1f) * z
+   + x
+   + 1.0;
+
+/* multiply by power of 2 */
+x = ceph_ldexpf( z, n );
+return( x );
+} 
+
 
 __ATTR_ALWAYS_INLINE__
 static inline
@@ -491,12 +594,5 @@ return(1.5707963267948966192f - ceph_asinf(x) );
 
 }
 }
-
-
-
-
-
-
-
 
 #endif /*__GMS_CEPHES_SIN_COS_H__*/
