b08d6dfdd1135f03c21c5633678fd428f9c5bc58
Bernard Gingold <beniekg@gmail.com>
2026-08-07 11:08:36 +0200

    Added: more Gaussian-Q function unit tests.
    Testing functionals of the Gaussian-Q function.

--- REOS_UnitTests/unit_test_integrands_funcs_ch4_sse.cpp
+++ REOS_UnitTests/unit_test_integrands_funcs_ch4_sse.cpp
@@ -313,10 +313,370 @@ void unit_test_integrand_4_6_sin_gauss_Q_func_sse_pd()
     printf_ret = std::printf("[UNIT-TEST:] -- of function=%s -- ENDED!!\n",__func__);
 }
 
+__attribute__((hot))
+__attribute__((aligned(32)))
+void unit_test_integrand_4_6_cos_gauss_Q_func_sse_pd();
+
+void unit_test_integrand_4_6_cos_gauss_Q_func_sse_pd()
+{
+    constexpr std::int32_t n_func_args{10};
+    constexpr std::int32_t n_gauss_q_vals{50};
+    constexpr std::int32_t tot_elems{n_func_args*n_gauss_q_vals};
+    constexpr double lo_x1{0.001};
+    constexpr double hi_x1{1.0};
+    constexpr double lo_y1{lo_x1};
+    constexpr double hi_y1{hi_x1};
+    constexpr double lo_rho{0.0};
+    constexpr double hi_rho{1.0};
+    __ATTR_ALIGN__(16) double in_buf[tot_elems];
+    __ATTR_ALIGN__(16) double out_buf[tot_elems];
+    __ATTR_ALIGN__(16) double rv_func_x1_buf[n_func_args];
+    __ATTR_ALIGN__(16) double rv_func_y1_buf[n_func_args];
+    __ATTR_ALIGN__(16) double rv_func_rho_buf[n_func_args];
+    __ATTR_ALIGN__(16) double rv_phi_arg_buf[n_func_args];
+    thread_local std::uniform_real_distribution<double> rv_func_arg_x1;
+    thread_local std::mt19937 rv_func_arg_x1_gen;
+    thread_local std::uint64_t seed_func_x1_arg{};
+    thread_local std::uniform_real_distribution<double> rv_func_arg_y1;
+    thread_local std::mt19937 rv_func_arg_y1_gen;
+    thread_local std::uint64_t seed_func_y1_arg{};
+    thread_local std::uniform_real_distribution<double> rv_func_arg_rho;
+    thread_local std::mt19937 rv_func_arg_rho_gen;
+    thread_local std::uint64_t seed_func_rho_arg{};
+    thread_local std::uniform_real_distribution<double> rv_gauss_q;
+    thread_local std::mt19937 rv_gauss_q_gen;
+    thread_local std::uint64_t seed_gauss_q{};
+    rv_func_arg_x1 = std::uniform_real_distribution<double>(lo_x1,hi_x1);
+    seed_func_x1_arg = __rdtsc();
+    rv_func_arg_x1_gen = std::mt19937(seed_func_x1_arg);
+    rv_func_arg_y1 = std::uniform_real_distribution<double>(lo_y1,hi_y1);
+    seed_func_y1_arg = __rdtsc();
+    rv_func_arg_y1_gen = std::mt19937(seed_func_y1_arg);
+    rv_func_arg_rho = std::uniform_real_distribution<double>(lo_rho,hi_rho);
+    seed_func_rho_arg = __rdtsc();
+    rv_func_arg_rho_gen = std::mt19937(seed_func_rho_arg);
+    [[maybe_unused]] std::int32_t printf_ret{};
+    printf_ret = std::printf("[UNIT-TEST:] -- of function=%s -- STARTED!!.\n",__func__);
+
+    for(std::int32_t i {0}; i < n_func_args; ++i) 
+    {
+        const std::int32_t outer_idx = i*n_gauss_q_vals;
+        const double x1 = rv_func_arg_x1.operator()(rv_func_arg_x1_gen);
+        rv_func_x1_buf[i] = x1;
+        const double y1 = rv_func_arg_y1.operator()(rv_func_arg_y1_gen);
+        rv_func_y1_buf[i] = y1;
+        const double rho= rv_func_arg_rho.operator()(rv_func_arg_rho_gen);
+        rv_func_rho_buf[i] = rho;
+        const double phi_arg = y1/x1;
+        rv_phi_arg_buf[i] = phi_arg;
+#if (UNIT_TEST_INTEGRANDS_FUNC_CH4_DBG_PRINT) == 1
+        printf_ret = print_double("phi_arg",phi_arg,0);
+#endif 
+        const double phi_s = std::atan(phi_arg);
+#if (UNIT_TEST_INTEGRANDS_FUNC_CH4_DBG_PRINT) == 1
+        printf_ret = print_double("phi_s",phi_s,0);
+#endif 
+        rv_gauss_q = std::uniform_real_distribution<double>(+0.0,+1.57079632679489662-phi_s);
+        seed_gauss_q = __rdtsc();
+        rv_gauss_q_gen = std::mt19937(seed_gauss_q);
+        for(std::int32_t j{0}; j < n_gauss_q_vals; ++j)   
+        {
+            const std::int32_t inner_idx = outer_idx+j;
+            const double theta = rv_gauss_q.operator()(rv_gauss_q_gen);
+            in_buf[inner_idx] = theta;
+        }
+    }
+    std::sort(&in_buf[0],&in_buf[tot_elems-1]);
+    std::sort(&rv_func_x1_buf[0],&rv_func_x1_buf[n_func_args-1]);
+    std::sort(&rv_func_y1_buf[0],&rv_func_y1_buf[n_func_args-1]);
+    std::sort(&rv_func_rho_buf[0],&rv_func_rho_buf[n_func_args-1]);
+    std::sort(&rv_phi_arg_buf[0],&rv_phi_arg_buf[n_func_args-1]);
+
+#if (UNIT_TEST_INTEGRANDS_FUNC_CH4_DBG_PRINT) == 0
+     for(std::int32_t i {0}; i < tot_elems; ++i) 
+     {  
+        if(i>=0 && i<=(n_func_args-1)) 
+        {
+          print_double("sort-check: rv_func_x1_buf",rv_func_x1_buf[i],0);
+          print_double("sort-check: rv_func_y1_buf",rv_func_y1_buf[i],0);
+          print_double("sort-check: rv_func_rho_buf",rv_func_rho_buf[i],0);
+          print_double("sort-check: rv_phi_arg_buf",rv_phi_arg_buf[i],0);
+        }
+        print_double("sort-check: in_buf=",in_buf[i],0);
+     }
+#endif 
+
+    for(std::int32_t i{0}; i < n_func_args; ++i) 
+    {  
+      const std::int32_t outer_idx = i*n_gauss_q_vals;
+      const double x1{rv_func_x1_buf[i]};
+      const __m128d vx1{_mm_set1_pd(x1)};
+      const double y1{rv_func_y1_buf[i]};
+      const __m128d vy1{_mm_set1_pd(y1)};
+      const double rho{rv_func_rho_buf[i]};
+      const __m128d vrho{_mm_set1_pd(rho)};
+      const double phiarg{rv_phi_arg_buf[i]};
+      const __m128d vphis{_mm_set1_pd(phiarg)};
+      for(std::int32_t j{0}; j < ROUND_DOWN(n_gauss_q_vals,1); j += 2) 
+      {
+          const std::int32_t inner_idx = outer_idx+j;
+          const __m128d theta{_mm_load_pd(&in_buf[inner_idx])};
+          const __m128d gauss_Q_res{gms::fading_channel::integrand_4_6_cos_gauss_Q_func_sse_pd(vx1,vy1,vrho,theta,vphis)};
+#if (UNIT_TEST_INTEGRANDS_FUNC_CH4_DBG_PRINT) == 1
+          const double * __restrict__ p_res{reinterpret_cast<const double * __restrict__>(&gauss_Q_res)};
+          printf_ret = print_double("p_res[0]",p_res[0],0);
+          printf_ret = print_double("p_res[1]",p_res[1],0);
+#endif 
+        _mm_store_pd(&out_buf[inner_idx],gauss_Q_res);
+      }
+    } 
+    printf_ret = std::printf("[UNIT-TEST:] -- of function=%s -- ENDED!!\n",__func__);
+}
+
+__attribute__((hot))
+__attribute__((aligned(32)))
+void unit_test_integrand_4_7_x1_gauss_Q_func_sse_pd();
+
+void unit_test_integrand_4_7_x1_gauss_Q_func_sse_pd()
+{
+    constexpr std::int32_t n_func_args{10};
+    constexpr std::int32_t n_gauss_q_vals{50};
+    constexpr std::int32_t tot_elems{n_func_args*n_gauss_q_vals};
+    constexpr double lo_x1{0.001};
+    constexpr double hi_x1{1.0};
+    constexpr double lo_y1{lo_x1};
+    constexpr double hi_y1{hi_x1};
+    constexpr double lo_rho{0.0};
+    constexpr double hi_rho{1.0};
+    __ATTR_ALIGN__(16) double in_buf[tot_elems];
+    __ATTR_ALIGN__(16) double out_buf[tot_elems];
+    __ATTR_ALIGN__(16) double rv_func_x1_buf[n_func_args];
+    __ATTR_ALIGN__(16) double rv_func_y1_buf[n_func_args];
+    __ATTR_ALIGN__(16) double rv_func_rho_buf[n_func_args];
+    __ATTR_ALIGN__(16) double rv_phi_arg_buf[n_func_args];
+    thread_local std::uniform_real_distribution<double> rv_func_arg_x1;
+    thread_local std::mt19937 rv_func_arg_x1_gen;
+    thread_local std::uint64_t seed_func_x1_arg{};
+    thread_local std::uniform_real_distribution<double> rv_func_arg_y1;
+    thread_local std::mt19937 rv_func_arg_y1_gen;
+    thread_local std::uint64_t seed_func_y1_arg{};
+    thread_local std::uniform_real_distribution<double> rv_func_arg_rho;
+    thread_local std::mt19937 rv_func_arg_rho_gen;
+    thread_local std::uint64_t seed_func_rho_arg{};
+    thread_local std::uniform_real_distribution<double> rv_gauss_q;
+    thread_local std::mt19937 rv_gauss_q_gen;
+    thread_local std::uint64_t seed_gauss_q{};
+    rv_func_arg_x1 = std::uniform_real_distribution<double>(lo_x1,hi_x1);
+    seed_func_x1_arg = __rdtsc();
+    rv_func_arg_x1_gen = std::mt19937(seed_func_x1_arg);
+    rv_func_arg_y1 = std::uniform_real_distribution<double>(lo_y1,hi_y1);
+    seed_func_y1_arg = __rdtsc();
+    rv_func_arg_y1_gen = std::mt19937(seed_func_y1_arg);
+    rv_func_arg_rho = std::uniform_real_distribution<double>(lo_rho,hi_rho);
+    seed_func_rho_arg = __rdtsc();
+    rv_func_arg_rho_gen = std::mt19937(seed_func_rho_arg);
+    [[maybe_unused]] std::int32_t printf_ret{};
+    printf_ret = std::printf("[UNIT-TEST:] -- of function=%s -- STARTED!!.\n",__func__);
+
+    for(std::int32_t i {0}; i < n_func_args; ++i) 
+    {
+        const std::int32_t outer_idx = i*n_gauss_q_vals;
+        const double x1 = rv_func_arg_x1.operator()(rv_func_arg_x1_gen);
+        rv_func_x1_buf[i] = x1;
+        const double y1 = rv_func_arg_y1.operator()(rv_func_arg_y1_gen);
+        rv_func_y1_buf[i] = y1;
+        const double rho= rv_func_arg_rho.operator()(rv_func_arg_rho_gen);
+        rv_func_rho_buf[i] = rho;
+        const double phi_arg = y1/x1;
+        rv_phi_arg_buf[i] = phi_arg;
+#if (UNIT_TEST_INTEGRANDS_FUNC_CH4_DBG_PRINT) == 1
+        printf_ret = print_double("phi_arg",phi_arg,0);
+#endif 
+        const double phi_s = std::atan(phi_arg);
+#if (UNIT_TEST_INTEGRANDS_FUNC_CH4_DBG_PRINT) == 1
+        printf_ret = print_double("phi_s",phi_s,0);
+#endif 
+        rv_gauss_q = std::uniform_real_distribution<double>(+0.0,+1.57079632679489662-phi_s);
+        seed_gauss_q = __rdtsc();
+        rv_gauss_q_gen = std::mt19937(seed_gauss_q);
+        for(std::int32_t j{0}; j < n_gauss_q_vals; ++j)   
+        {
+            const std::int32_t inner_idx = outer_idx+j;
+            const double theta = rv_gauss_q.operator()(rv_gauss_q_gen);
+            in_buf[inner_idx] = theta;
+        }
+    }
+    std::sort(&in_buf[0],&in_buf[tot_elems-1]);
+    std::sort(&rv_func_x1_buf[0],&rv_func_x1_buf[n_func_args-1]);
+    // Not used and left for verification.
+    std::sort(&rv_func_y1_buf[0],&rv_func_y1_buf[n_func_args-1]);
+    std::sort(&rv_func_rho_buf[0],&rv_func_rho_buf[n_func_args-1]);
+    // Not used and left for verification.
+    std::sort(&rv_phi_arg_buf[0],&rv_phi_arg_buf[n_func_args-1]);
+
+#if (UNIT_TEST_INTEGRANDS_FUNC_CH4_DBG_PRINT) == 0
+     for(std::int32_t i {0}; i < tot_elems; ++i) 
+     {  
+        if(i>=0 && i<=(n_func_args-1)) 
+        {
+          print_double("sort-check: rv_func_x1_buf",rv_func_x1_buf[i],0);
+          print_double("sort-check: rv_func_y1_buf",rv_func_y1_buf[i],0);
+          print_double("sort-check: rv_func_rho_buf",rv_func_rho_buf[i],0);
+          print_double("sort-check: rv_phi_arg_buf",rv_phi_arg_buf[i],0);
+        }
+        print_double("sort-check: in_buf=",in_buf[i],0);
+     }
+#endif 
+
+    for(std::int32_t i{0}; i < n_func_args; ++i) 
+    {  
+      const std::int32_t outer_idx = i*n_gauss_q_vals;
+      const double x1{rv_func_x1_buf[i]};
+      const __m128d vx1{_mm_set1_pd(x1)};
+      const double rho{rv_func_rho_buf[i]};
+      const __m128d vrho{_mm_set1_pd(rho)};
+      for(std::int32_t j{0}; j < ROUND_DOWN(n_gauss_q_vals,1); j += 2) 
+      {
+          const std::int32_t inner_idx = outer_idx+j;
+          const __m128d theta{_mm_load_pd(&in_buf[inner_idx])};
+          const __m128d gauss_Q_res{gms::fading_channel::integrand_4_7_x1_gauss_Q_func_sse_pd(vx1,vrho,theta)};
+#if (UNIT_TEST_INTEGRANDS_FUNC_CH4_DBG_PRINT) == 1
+          const double * __restrict__ p_res{reinterpret_cast<const double * __restrict__>(&gauss_Q_res)};
+          printf_ret = print_double("p_res[0]",p_res[0],0);
+          printf_ret = print_double("p_res[1]",p_res[1],0);
+#endif 
+        _mm_store_pd(&out_buf[inner_idx],gauss_Q_res);
+      }
+    } 
+    printf_ret = std::printf("[UNIT-TEST:] -- of function=%s -- ENDED!!\n",__func__);
+}
+
+__attribute__((hot))
+__attribute__((aligned(32)))
+void unit_test_integrand_4_7_y1_gauss_Q_func_sse_pd();
+
+void unit_test_integrand_4_7_y1_gauss_Q_func_sse_pd()
+{
+    constexpr std::int32_t n_func_args{10};
+    constexpr std::int32_t n_gauss_q_vals{50};
+    constexpr std::int32_t tot_elems{n_func_args*n_gauss_q_vals};
+    constexpr double lo_x1{0.001};
+    constexpr double hi_x1{1.0};
+    constexpr double lo_y1{lo_x1};
+    constexpr double hi_y1{hi_x1};
+    constexpr double lo_rho{0.0};
+    constexpr double hi_rho{1.0};
+    __ATTR_ALIGN__(16) double in_buf[tot_elems];
+    __ATTR_ALIGN__(16) double out_buf[tot_elems];
+    __ATTR_ALIGN__(16) double rv_func_x1_buf[n_func_args];
+    __ATTR_ALIGN__(16) double rv_func_y1_buf[n_func_args];
+    __ATTR_ALIGN__(16) double rv_func_rho_buf[n_func_args];
+    __ATTR_ALIGN__(16) double rv_phi_arg_buf[n_func_args];
+    thread_local std::uniform_real_distribution<double> rv_func_arg_x1;
+    thread_local std::mt19937 rv_func_arg_x1_gen;
+    thread_local std::uint64_t seed_func_x1_arg{};
+    thread_local std::uniform_real_distribution<double> rv_func_arg_y1;
+    thread_local std::mt19937 rv_func_arg_y1_gen;
+    thread_local std::uint64_t seed_func_y1_arg{};
+    thread_local std::uniform_real_distribution<double> rv_func_arg_rho;
+    thread_local std::mt19937 rv_func_arg_rho_gen;
+    thread_local std::uint64_t seed_func_rho_arg{};
+    thread_local std::uniform_real_distribution<double> rv_gauss_q;
+    thread_local std::mt19937 rv_gauss_q_gen;
+    thread_local std::uint64_t seed_gauss_q{};
+    rv_func_arg_x1 = std::uniform_real_distribution<double>(lo_x1,hi_x1);
+    seed_func_x1_arg = __rdtsc();
+    rv_func_arg_x1_gen = std::mt19937(seed_func_x1_arg);
+    rv_func_arg_y1 = std::uniform_real_distribution<double>(lo_y1,hi_y1);
+    seed_func_y1_arg = __rdtsc();
+    rv_func_arg_y1_gen = std::mt19937(seed_func_y1_arg);
+    rv_func_arg_rho = std::uniform_real_distribution<double>(lo_rho,hi_rho);
+    seed_func_rho_arg = __rdtsc();
+    rv_func_arg_rho_gen = std::mt19937(seed_func_rho_arg);
+    [[maybe_unused]] std::int32_t printf_ret{};
+    printf_ret = std::printf("[UNIT-TEST:] -- of function=%s -- STARTED!!.\n",__func__);
+
+    for(std::int32_t i {0}; i < n_func_args; ++i) 
+    {
+        const std::int32_t outer_idx = i*n_gauss_q_vals;
+        const double x1 = rv_func_arg_x1.operator()(rv_func_arg_x1_gen);
+        rv_func_x1_buf[i] = x1;
+        const double y1 = rv_func_arg_y1.operator()(rv_func_arg_y1_gen);
+        rv_func_y1_buf[i] = y1;
+        const double rho= rv_func_arg_rho.operator()(rv_func_arg_rho_gen);
+        rv_func_rho_buf[i] = rho;
+        const double phi_arg = y1/x1;
+        rv_phi_arg_buf[i] = phi_arg;
+#if (UNIT_TEST_INTEGRANDS_FUNC_CH4_DBG_PRINT) == 1
+        printf_ret = print_double("phi_arg",phi_arg,0);
+#endif 
+        const double phi_s = std::atan(phi_arg);
+#if (UNIT_TEST_INTEGRANDS_FUNC_CH4_DBG_PRINT) == 1
+        printf_ret = print_double("phi_s",phi_s,0);
+#endif 
+        rv_gauss_q = std::uniform_real_distribution<double>(+0.0,+1.57079632679489662-phi_s);
+        seed_gauss_q = __rdtsc();
+        rv_gauss_q_gen = std::mt19937(seed_gauss_q);
+        for(std::int32_t j{0}; j < n_gauss_q_vals; ++j)   
+        {
+            const std::int32_t inner_idx = outer_idx+j;
+            const double theta = rv_gauss_q.operator()(rv_gauss_q_gen);
+            in_buf[inner_idx] = theta;
+        }
+    }
+    std::sort(&in_buf[0],&in_buf[tot_elems-1]);
+    std::sort(&rv_func_x1_buf[0],&rv_func_x1_buf[n_func_args-1]);
+    // Not used and left for verification.
+    std::sort(&rv_func_y1_buf[0],&rv_func_y1_buf[n_func_args-1]);
+    std::sort(&rv_func_rho_buf[0],&rv_func_rho_buf[n_func_args-1]);
+    // Not used and left for verification.
+    std::sort(&rv_phi_arg_buf[0],&rv_phi_arg_buf[n_func_args-1]);
+
+#if (UNIT_TEST_INTEGRANDS_FUNC_CH4_DBG_PRINT) == 0
+     for(std::int32_t i {0}; i < tot_elems; ++i) 
+     {  
+        if(i>=0 && i<=(n_func_args-1)) 
+        {
+          print_double("sort-check: rv_func_x1_buf",rv_func_x1_buf[i],0);
+          print_double("sort-check: rv_func_y1_buf",rv_func_y1_buf[i],0);
+          print_double("sort-check: rv_func_rho_buf",rv_func_rho_buf[i],0);
+          print_double("sort-check: rv_phi_arg_buf",rv_phi_arg_buf[i],0);
+        }
+        print_double("sort-check: in_buf=",in_buf[i],0);
+     }
+#endif 
+
+    for(std::int32_t i{0}; i < n_func_args; ++i) 
+    {  
+      const std::int32_t outer_idx = i*n_gauss_q_vals;
+      const double y1{rv_func_y1_buf[i]};
+      const __m128d vy1{_mm_set1_pd(y1)};
+      const double rho{rv_func_rho_buf[i]};
+      const __m128d vrho{_mm_set1_pd(rho)};
+      for(std::int32_t j{0}; j < ROUND_DOWN(n_gauss_q_vals,1); j += 2) 
+      {
+          const std::int32_t inner_idx = outer_idx+j;
+          const __m128d theta{_mm_load_pd(&in_buf[inner_idx])};
+          const __m128d gauss_Q_res{gms::fading_channel::integrand_4_7_y1_gauss_Q_func_sse_pd(vy1,vrho,theta)};
+#if (UNIT_TEST_INTEGRANDS_FUNC_CH4_DBG_PRINT) == 1
+          const double * __restrict__ p_res{reinterpret_cast<const double * __restrict__>(&gauss_Q_res)};
+          printf_ret = print_double("p_res[0]",p_res[0],0);
+          printf_ret = print_double("p_res[1]",p_res[1],0);
+#endif 
+        _mm_store_pd(&out_buf[inner_idx],gauss_Q_res);
+      }
+    } 
+    printf_ret = std::printf("[UNIT-TEST:] -- of function=%s -- ENDED!!\n",__func__);
+}
+
+
 int main()
 {
     (void)unit_test_integrand_4_1_gauss_Q_func_sse_pd();
     (void)unit_test_integrand_4_2_gauss_Q_func_sse_pd();
     (void)unit_test_integrand_4_6_sin_gauss_Q_func_sse_pd();
+    (void)unit_test_integrand_4_6_cos_gauss_Q_func_sse_pd();
+    (void)unit_test_integrand_4_7_x1_gauss_Q_func_sse_pd();
+    (void)unit_test_integrand_4_7_y1_gauss_Q_func_sse_pd();
     return 0;
 }
