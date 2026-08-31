8686d89e486c1852142c63a091b5b2150c6773a4
Bernard Gingold <beniekg@gmail.com>
2026-08-31 09:16:50 +0200

    Added: compute functional of  Rice Fading Channel.
    Formula 5.12

--- src/fading_channel/GMS_compute_functionals_ch5.h
+++ src/fading_channel/GMS_compute_functionals_ch5.h
@@ -242,6 +242,16 @@ __ATTR_ALIGN__(32)
 std::int32_t
 compute_functionals_Hoyt_chan_5_9(quadpack_integrator_payload_ch5_t * __restrict__);
 
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+#pragma intel optimization_level 3 
+#elif defined (__GNUC__) && (!defined (__INTEL_COMPILER) || !defined(__ICC))
+#pragma GCC optimize("O3")
+#endif
+__ATTR_HOT__
+__ATTR_ALIGN__(32)
+std::int32_t
+compute_functionals_Rice_chan_5_12(quadpack_integrator_payload_ch5_t * __restrict__);
+
 
 } // fading_channel
 
