17f32f0b785064cc47fdb30cb9e2f3ceb17e9e0d
Bernard Gingold <beniekg@gmail.com>
2026-09-05 17:49:51 +0200

    Update GMS_swap_avx_unroll16x.cpp

--- src/kernels/BLAS-kernels/GMS_swap_avx_unroll16x.cpp
+++ src/kernels/BLAS-kernels/GMS_swap_avx_unroll16x.cpp
@@ -6,7 +6,7 @@
 /*
      This version is based on BLIS framework original implementation.
      Few changes were introduced in order to more easily adapt it 
-     to 'Guided Missile Simulation' project.
+     to 'RF-EMT' project.
      
    The original authors license
    BLIS
@@ -38,7 +38,22 @@
    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
      
 */
-
+/* Copyright (C) Bernard Gingold, 2020-2026 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
 
 #include <immintrin.h>
 #include <omp.h>
