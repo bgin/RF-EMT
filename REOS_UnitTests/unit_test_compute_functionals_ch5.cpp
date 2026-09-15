8179a0d10377084b7f3197cc8130f2612fdf776b
Bernard Gingold <beniekg@gmail.com>
2026-09-15 13:44:14 +0200

    OpenMP processing of Nakagami-m channel functional.
    unit test.
    Formula: 5.59

--- REOS_UnitTests/unit_test_compute_functionals_ch5.cpp
+++ REOS_UnitTests/unit_test_compute_functionals_ch5.cpp
@@ -5,7 +5,7 @@
 #include "GMS_omp_utils.h"
 
 /*
-   icpc -o unit_test_compute_functionals_ch5 -fopenmp -O3 -fp-model fast=2 -fno-exceptions -std=c++17 -ftz -ggdb -ipo -march=skylake-avx512 -mavx512f -falign-functions=32 -w1 -qopt-report=5  \
+   icpc -o unit_test_compute_functionals_ch5 -fopenmp -O3 -fp-model -fasm-blocks fast=2 -fno-exceptions -std=c++17 -ftz -ggdb -ipo -march=skylake-avx512 -mavx512f -falign-functions=32 -w1 -qopt-report=5  \
    GMS_config.h GMS_cephes_double.h GMS_hypergeometric_func.h GMS_integrands_func_ch5.h GMS_integrands_func_ch5.cpp GMS_cquadpack.h GMS_cquadpack.c GMS_tabulated_quadrature.h GMS_tabulated_quadrature.cpp GMS_machine_utils.h GMS_omp_utils.h GMS_omp_utils.cpp GMS_compute_functionals_ch5.h GMS_compute_functionals_ch5.cpp unit_test_compute_functionals_ch5.cpp
    ASM: 
    icpc -S -O3 -fopenmp -fverbose-asm -masm=intel -fno-exceptions -std=c++17 -march=skylake-avx512 -mavx512f -falign-functions=32 GMS_config.h GMS_cephes_double.h GMS_integrands_func_ch5.h GMS_integrands_func_ch5.cpp GMS_machine_utils.h GMS_compute_functionals_ch5.h GMS_compute_functionals_ch5.cpp unit_test_compute_functionals_ch5.cpp
@@ -1674,7 +1674,7 @@ void unit_test_compute_functional_Hoyt_LaplaceT_chan_5_56()
     inf[0]    = 1;
     irule[0]  = 4;
     gms::fading_channel::quadpack_integrator_payload_ch5_t integrator_payload;
-    integrator_payload.integrand = &gms::fading_channel::integrand_Hoyt_lt_chan_5_40_iface;
+    integrator_payload.integrand = &gms::fading_channel::integrand_Hoyt_lt_chan_5_56_iface;
     integrator_payload.func_args_payload = &func_args[0];
     integrator_payload.tmp_work1 = &tmp_work1[0];
     integrator_payload.tmp_work2 = &tmp_work2[0];
@@ -1810,7 +1810,7 @@ void unit_test_compute_functional_Rice_LaplaceT_chan_5_57()
     inf[0]    = 1;
     irule[0]  = 4;
     gms::fading_channel::quadpack_integrator_payload_ch5_t integrator_payload;
-    integrator_payload.integrand = &gms::fading_channel::integrand_Rice_lt_chan_5_41_iface;
+    integrator_payload.integrand = &gms::fading_channel::integrand_Rice_lt_chan_5_57_iface;
     integrator_payload.func_args_payload = &func_args[0];
     integrator_payload.tmp_work1 = &tmp_work1[0];
     integrator_payload.tmp_work2 = &tmp_work2[0];
@@ -1889,7 +1889,7 @@ void unit_test_compute_functional_Rice_LaplaceT_chan_5_57()
         {
            const std::string append_integrator_name{gms::fading_channel::integrators_names_ch5[integrators_cnt].c_str()};
            gms::fading_channel::create_functional_ch5_plot(p_payload->n_func_vals,nullptr,&p_payload->functional[0],
-                                                           "unit_test_omp_compute_functional_Rice_LaplaceT_chan_5_47_"+append_integrator_name,
+                                                           "unit_test_omp_compute_functional_Rice_LaplaceT_chan_5_57_"+append_integrator_name,
                                                            "Functional of Rice Fading Channel computed by:"+append_integrator_name,false);
            print_retv = std::printf("OpenMP processing for: %s, consumed=%llu(TSC)\n",append_integrator_name.c_str(),p_payload->omp_loop_delta[0]);
                                                            
@@ -1908,6 +1908,143 @@ void unit_test_compute_functional_Rice_LaplaceT_chan_5_57()
     print_retv = std::printf("[UNIT-TEST:] -- of function=%s -- ENDED!!\n",__func__);
 }
 
+__attribute__((hot))
+__attribute__((aligned(32)))
+void unit_test_compute_functional_Nakagami_m_LaplaceT_chan_5_59();
+
+void unit_test_compute_functional_Nakagami_m_LaplaceT_chan_5_59()
+{
+    //using namespace gms::fading_channel;
+    constexpr std::int32_t n_func_args{100};
+    [[maybe_unused]] __ATTR_ALIGN__(64) gms::fading_channel::func_args_ch5_payload_t func_args[n_func_args]; // unused
+    __ATTR_ALIGN__(16) double Nakagami_m_LaplaceT_Channel_functional[n_func_args];
+    __ATTR_ALIGN__(16) double abser[n_func_args];
+    __ATTR_ALIGN__(16) double tmp_work1[n_func_args];
+    __ATTR_ALIGN__(16) double tmp_work2[n_func_args];
+    __ATTR_ALIGN__(16) double tmp_work3[n_func_args];
+    __ATTR_ALIGN__(16) double tmp_work4[n_func_args];
+    __ATTR_ALIGN__(16) std::uint64_t crude_tsc_results[n_func_args];
+    __ATTR_ALIGN__(16) std::uint64_t crude_tsc_start[n_func_args];
+    __ATTR_ALIGN__(16) std::uint64_t crude_tsc_end[n_func_args];
+    __ATTR_ALIGN__(16) std::int32_t neval[n_func_args];
+    __ATTR_ALIGN__(16) std::int32_t ier[n_func_args];
+    __ATTR_ALIGN__(16) std::int32_t last[n_func_args];
+#if (COMPUTE_FUNCTIONALS_CH5_PARALLELIZE_QUADPACK_CALLS) == 0
+                       std::uint64_t omp_loop_start[1];
+                       std::uint64_t omp_loop_end[1];
+                       std::uint64_t omp_loop_delta[1];
+#endif
+    double epsabs[1];
+    double epsrel[1];
+    std::int32_t inf[1];
+    std::int32_t irule[1];
+    [[maybe_unused]] std::int32_t print_retv{};
+    print_retv = std::printf("[UNIT-TEST:] -- of function=%s -- STARTED!!.\n",__func__);
+    epsabs[0] = 0.0;
+    epsrel[0] = 0.001;
+    inf[0]    = 1;
+    irule[0]  = 4;
+    gms::fading_channel::quadpack_integrator_payload_ch5_t integrator_payload;
+    integrator_payload.integrand = &gms::fading_channel::integrand_Nakagami_m_lt_chan_5_59_iface;
+    integrator_payload.func_args_payload = &func_args[0];
+    integrator_payload.tmp_work1 = &tmp_work1[0];
+    integrator_payload.tmp_work2 = &tmp_work2[0];
+    integrator_payload.tmp_work3 = &tmp_work3[0];
+    integrator_payload.tmp_work4 = &tmp_work4[0];
+    integrator_payload.inf   = &inf[0];
+    integrator_payload.irule = &irule[0];
+    integrator_payload.epsabs = &epsabs[0];
+    integrator_payload.epsrel = &epsrel[0];
+    integrator_payload.abser  = &abser[0];
+    integrator_payload.functional = &Nakagami_m_LaplaceT_Channel_functional[0];
+    integrator_payload.neval = &neval[0];
+    integrator_payload.ier = &ier[0];
+    integrator_payload.last = &last[0];
+    integrator_payload.crude_tsc_start = &crude_tsc_start[0];
+    integrator_payload.crude_tsc_end   = &crude_tsc_end[0];
+    integrator_payload.crude_tsc_measurement = &crude_tsc_results[0];
+#if (COMPUTE_FUNCTIONALS_CH5_PARALLELIZE_QUADPACK_CALLS) == 1
+    integrator_payload.omp_loop_start = &omp_loop_start[0];
+    integrator_payload.omp_loop_end   = &omp_loop_end[0];
+    integrator_payload.omp_loop_delta = &omp_loop_delta[0];
+#endif
+    integrator_payload.rand_lo1 = +0.1;
+    integrator_payload.rand_hi1 = +1.0;
+    integrator_payload.rand_lo2 = +1.5;//m
+    integrator_payload.rand_hi2 = +5.0;//m
+    integrator_payload.rand_lo3 = +2.0;
+    integrator_payload.rand_hi3 = +7.0;
+    integrator_payload.rand_lo4 = +7.5;//l 
+    integrator_payload.rand_hi4 = +10.77545;//l
+    integrator_payload.n_func_vals = n_func_args;
+    integrator_payload.randomly_generate_inputs = true;
+#if (COMPUTE_FUNCTIONALS_CH5_PARALLELIZE_QUADPACK_CALLS) == 1
+    integrator_payload.set_n_threads = 6;
+    integrator_payload.omp_env_settings.omp_places_values = "{0,1,2,3,4,5}";
+    integrator_payload.omp_env_settings.omp_display_env_values = "VERBOSE";
+    integrator_payload.omp_env_settings.omp_proc_bind_values ="spread";
+
+    std::int32_t setenv_ret;
+    setenv_ret = setenv(integrator_payload.omp_env_settings.set_omp_places,integrator_payload.omp_env_settings.omp_places_values,1);
+    if(setenv_ret==-1)
+    {
+      print_retv = std::printf("Function=%s,line=%d\n",__func__,__LINE__);
+      (void)perror("***ERROR*** in: setenv -- ");
+    }
+    setenv_ret = setenv(integrator_payload.omp_env_settings.set_omp_display_env,integrator_payload.omp_env_settings.omp_display_env_values,1);
+    if(setenv_ret==-1)
+    {
+      print_retv = std::printf("Function=%s,line=%d\n",__func__,__LINE__);
+      (void)perror("***ERROR*** in: setenv -- ");
+    }
+    setenv_ret = setenv(integrator_payload.omp_env_settings.set_omp_proc_bind,integrator_payload.omp_env_settings.omp_proc_bind_values,1);
+    if(setenv_ret==-1)
+    {
+      print_retv = std::printf("Function=%s,line=%d\n",__func__,__LINE__);
+      (void)perror("***ERROR*** in: setenv -- ");
+    }
+#endif 
+    //integrator_payload.which_integrator = 3;
+    gms::fading_channel::quadpack_integrator_payload_ch5_t * __restrict__ p_payload = &integrator_payload;
+    std::int32_t integrators_cnt = 0;
+    for(std::int32_t ii{1}; ii<5; ++ii) 
+    {   
+        ++integrators_cnt;
+        integrator_payload.which_integrator = ii;
+        std::int32_t integrator_ret = gms::fading_channel::compute_functional_Nakagami_m_LaplaceT_chan_5_59(p_payload);
+        if(p_payload->which_integrator==1)
+           print_retv = std::printf("[UNIT-TEST:] -- currently executing: %s integrator\n",gms::fading_channel::integrators_names_ch5[0].c_str());
+        else if(p_payload->which_integrator==2)
+           print_retv = std::printf("[UNIT-TEST:] -- 'DQAGI' Not-Supported!!\n");
+        else if(p_payload->which_integrator==3)
+           print_retv = std::printf("[UNIT-TEST:] -- currently executing: %s integrator\n",gms::fading_channel::integrators_names_ch5[2].c_str());
+        else if(p_payload->which_integrator==4)
+           print_retv = std::printf("[UNIT-TEST:] -- currently executing: %s integrator\n",gms::fading_channel::integrators_names_ch5[3].c_str());
+        if(p_payload->which_integrator!=2)
+        {
+           const std::string append_integrator_name{gms::fading_channel::integrators_names_ch5[integrators_cnt].c_str()};
+           gms::fading_channel::create_functional_ch5_plot(p_payload->n_func_vals,nullptr,&p_payload->functional[0],
+                                                           "unit_test_omp_compute_functional_Nakagami_m_LaplaceT_chan_5_59_"+append_integrator_name,
+                                                           "Functional of Nakagami-m Fading Channel computed by:"+append_integrator_name,false);
+
+           print_retv = std::printf("OpenMP processing for: %s, consumed=%llu(TSC)\n",append_integrator_name.c_str(),p_payload->omp_loop_delta[0]);
+                                                           
+           for(std::int32_t i{0}; i < p_payload->n_func_vals; ++i) 
+           {
+               print_retv = print_double("Nakagami_m-Fading Channel (f:5.59)",p_payload->functional[i],0);
+           }
+           for(std::int32_t i{0}; i < p_payload->n_func_vals; ++i) 
+           {
+               const std::uint64_t start = crude_tsc_start[i];
+               const std::uint64_t end   = crude_tsc_end[i];
+               print_retv = std::printf("Iter=%d,abser=%.17f,neval=%d,ier=%d,last=%d,tsc_start=%llu,tsc_end=%llu,total_tsc=%llu,ret=%d\n",i,abser[i],neval[i],ier[i],last[i],start,end,crude_tsc_results[i],integrator_ret);
+           }
+        }
+    }
+    print_retv = std::printf("[UNIT-TEST:] -- of function=%s -- ENDED!!\n",__func__);
+}
+
+
 
 int main()
 {
@@ -1925,6 +2062,8 @@ int main()
   // (void)unit_test_compute_functional_Nakagami_m_LaplaceT_chan_5_43();
   // (void)unit_test_compute_functional_compositeLogNormShadow_chan_5_44();
   //(void)unit_test_compute_functional_Hoyt_LaplaceT_chan_5_56();
-  (void)unit_test_compute_functional_Rice_LaplaceT_chan_5_57();
+  //(void)unit_test_compute_functional_Rice_LaplaceT_chan_5_57();
+  (void)unit_test_compute_functional_Nakagami_m_LaplaceT_chan_5_59();
+
    return 0;
 }
