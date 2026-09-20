f85eaf0345755ea951199fe8154f53c5b580aaa5
Bernard Gingold <beniekg@gmail.com>
2026-09-20 06:02:22 +0200

    Update GMS_axpy_kernels.cu

--- src/gpu/kernels/GMS_axpy_kernels.cu
+++ src/gpu/kernels/GMS_axpy_kernels.cu
@@ -35,7 +35,22 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 // Work per thread      : WORKSIZE_n
 // Shared mem per block : 0
 //
-
+/* Copyright (C) Bernard Gingold, 2020-2026 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
 __global__ void axpy_kernel_r4(float alpha, 
                               const float*  __restrict__ x, 
                               float*  __restrict__ y, 
