adf37f7e91eddba2c4eea43282bf03a5989de6d7
Bernard Gingold <beniekg@gmail.com>
2026-09-22 06:09:02 +0200

    Implemented: 'OQPSK_param_b2_8_64'.

--- src/fading_channel/GMS_analytic_bep_sep_ch8.h
+++ src/fading_channel/GMS_analytic_bep_sep_ch8.h
@@ -279,6 +279,28 @@ _Tp OQPSK_param_a2_8_64(const _Tp Ac,const _Tp Ts,
 #pragma GCC optimize("O3")
 #pragma GCC target("sse")
 #endif
+#endif
+template<typename _Tp>
+__ATTR_ALWAYS_INLINE__
+static inline 
+_Tp OQPSK_param_b2_8_64(const _Tp Ac,const _Tp Ts,
+                        const _Tp M, const _Tp N0,
+                        const _Tp Bl,const _Tp Tb)
+{
+    const _Tp Eb = avg_bit_E_to_carrier_A<_Tp>(Ac,Ts,M);
+    const _Tp G  = G_PLL_bandwidth<_Tp>(Bl,Tb);
+    const _Tp fac= (G+static_cast<_Tp>(1.0))+std::sqrt<_Tp>(G+G);
+    return ((Eb/N0)*fac);
+}
+
+#if (ANALYTIC_BEP_SEP_CH8_OVERRIDE_COMPILER_CMD_LINE) == 1
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+#pragma intel optimization_level 3 
+#pragma intel optimization_parameter target_arch=SSE
+#elif defined (__GNUC__) && (!defined (__INTEL_COMPILER) || !defined(__ICC))
+#pragma GCC optimize("O3")
+#pragma GCC target("sse")
+#endif
 #endif 
 template<Gaussian_Q_approxmations_t Q_func_approx>
 __ATTR_ALWAYS_INLINE__
